7a6ec6f7bd478aed32022feec24031331a28d2d9
sangpok <48979587+sangpok@users.noreply.github.com>
2024-04-26 20:31:21 +0900

    [GONG-31] remove: NextAuth 제거 백엔드에서 제공하는 로그인이 생소한 방식이라 고민 좀 해봐야 할 것 같음 일단 NextAuth로는 안 되는 듯 하여 제거함

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { cn } from '@/lib/utils';
-import NextAuthProvider from '@/components/NextAuthProvider/NextAuthProvider';
 
 const pretendard = localFont({
   src: '../static/fonts/PretendardVariable.woff2',
@@ -23,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={cn(pretendard.className, '')}>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        {children}
       </body>
     </html>
   );
