4e6fc1d06126bba82ace5d901fecf2d115037314
sangpok <48979587+sangpok@users.noreply.github.com>
2024-05-01 21:12:38 +0900

    [GONG-41] design: 대시보드 레이아웃 적용

--- src/app/class/[cid]/dashboard/layout.tsx
+++ src/app/class/[cid]/dashboard/layout.tsx
@@ -13,26 +13,28 @@ export default function Layout({
   todayRank: ReactNode;
 }) {
   return (
-    <section>
+    <section className="flex flex-col gap-3 md:gap-6">
       <div className="flex flex-row gap-2 p-3 items-start bg-green-50 rounded md:gap-6">
         <div className="flex flex-row gap-1 min-w-fit items-center">
-          <Megaphone className="size-4" />
+          <Megaphone className="size-6 p-1" />
           <span className="font-semibold hidden md:block">공지사항</span>
         </div>
 
-        <span className="leading-none md:leading-normal">
+        <span>
           <strong>황영웅</strong>(이)가 <strong>7시간 동안 집중</strong>하는 중입니다! 미쳐날뛰고 있군요!
         </span>
       </div>
 
-      <div>
-        {recentFocus}
-        {groupFocus}
-      </div>
+      <div className="flex flex-col md:flex-row-reverse">
+        <div className="flex-1">
+          {todayRank}
+          {timetable}
+        </div>
 
-      <div>
-        {todayRank}
-        {timetable}
+        <div className="flex-1">
+          {recentFocus}
+          {groupFocus}
+        </div>
       </div>
     </section>
   );
