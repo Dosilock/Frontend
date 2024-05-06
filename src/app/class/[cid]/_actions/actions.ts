8e43ba14d916ab89be34468645f165cd41b39dc3
sangpok <48979587+sangpok@users.noreply.github.com>
2024-05-06 22:02:45 +0900

    [GONG-50] refactor: 서버시간 및 시간표 정보 얻어오는 Action 분리

--- src/app/class/[cid]/_actions/actions.ts
+++ src/app/class/[cid]/_actions/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { Period } from '../_store/usePeriods';
+import { DayOfWeek } from '../_store/TimetableStore';
 
 const requestEchoAPI = <T>(result: T) => {
   return new Promise<T>((resolve) => {
@@ -11,23 +11,21 @@ const requestEchoAPI = <T>(result: T) => {
   });
 };
 
-enum DayOfWeek {
-  MONDAY = 0,
-  TUESDAY = 1,
-  WEDNESDAY = 2,
-  THURSDAY = 3,
-  FRIDAY = 4,
-  SATURDAY = 5,
-  SUNDAY = 6,
-}
-
-type Timetable = {
+export type TimetableResponse = {
   name: string;
   dayOfWeeks: DayOfWeek[];
-  periods: Period[];
+  periods: InitialPeriod[];
 };
 
-const _timetable: Timetable = {
+export type InitialPeriod = {
+  id: number;
+  name: string;
+  startTime: string; // HH:mm 형식
+  duration: number;
+  isAttendacneRequired: boolean;
+};
+
+const _timetable: TimetableResponse = {
   name: '열심히 살자',
   dayOfWeeks: [
     DayOfWeek.MONDAY,
@@ -39,13 +37,18 @@ const _timetable: Timetable = {
     DayOfWeek.SUNDAY,
   ],
   periods: [
-    { id: 0, name: '오전1', startTime: new Date('2024-05-04 09:00:00'), duration: 180, isAttendacneRequired: false },
-    { id: 1, name: '점심 시간', startTime: new Date('2024-05-04 12:00:00'), duration: 90, isAttendacneRequired: false },
-    { id: 2, name: '오후1', startTime: new Date('2024-05-04 13:30:00'), duration: 240, isAttendacneRequired: false },
-    { id: 3, name: '저녁 시간', startTime: new Date('2024-05-04 17:30:00'), duration: 90, isAttendacneRequired: false },
-    { id: 4, name: '오후2', startTime: new Date('2024-05-04 19:00:00'), duration: 240, isAttendacneRequired: false },
+    { id: 0, name: '오전1', startTime: '09:00', duration: 180, isAttendacneRequired: false },
+    { id: 1, name: '점심 시간', startTime: '12:00', duration: 90, isAttendacneRequired: false },
+    { id: 2, name: '오후1', startTime: '13:30', duration: 240, isAttendacneRequired: false },
+    { id: 3, name: '저녁 시간', startTime: '17:30', duration: 90, isAttendacneRequired: false },
+    { id: 4, name: '오후2', startTime: '19:00', duration: 240, isAttendacneRequired: false },
+    // { id: 0, name: '오전1', startTime: new Date('2024-05-04 09:00:00'), duration: 180, isAttendacneRequired: false },
+    // { id: 1, name: '점심 시간', startTime: new Date('2024-05-04 12:00:00'), duration: 90, isAttendacneRequired: false },
+    // { id: 2, name: '오후1', startTime: new Date('2024-05-04 13:30:00'), duration: 240, isAttendacneRequired: false },
+    // { id: 3, name: '저녁 시간', startTime: new Date('2024-05-04 17:30:00'), duration: 90, isAttendacneRequired: false },
+    // { id: 4, name: '오후2', startTime: new Date('2024-05-04 19:00:00'), duration: 240, isAttendacneRequired: false },
   ],
 };
 
-export const getServerTime = async () => requestEchoAPI(new Date('2024-05-04 12:45:00'));
+export const getServerTime = async () => requestEchoAPI(new Date('2024-05-07 04:45:00'));
 export const getTimetable = async () => requestEchoAPI(_timetable);
