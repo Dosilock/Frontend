71dbf53a40e509059848c781ec5c272a6fd65e25
김 주현 <48979587+sangpok@users.noreply.github.com>
2024-05-16 16:05:26 +0900

    ✨ Socket.io 실시간 통신 기능 기본 설정 (#48)
    * ➕ Socket.io Package 추가 (#44)
    
    * ✨ Socket.io Instance 모듈 작성 (#44)
    
    Socket.io 인스턴스를 반환하는 모듈 작성
    
    * ✨ Socket 이벤트 Enum 작성 (#44)
    
    Socket 기본 이벤트에 필요한 이름을 작성
    
    * ✨ Socket 이벤트 관리하는 SocketHandler 작성 (#44)
    
    Socket 이벤트 콜백함수를 관리하는 SocketHandler 작성

---
+++ src/app/class/[cid]/_handler/SocketHandler.tsx
@@ -0,0 +1,19 @@
+import { socket } from '@/socket/socket';
+import { useEffect } from 'react';
+
+export const SocketHandler = () => {
+  useEffect(() => {
+    const handleConnect = () => {};
+    const handleDisconnect = () => {};
+
+    socket.on('connect', handleConnect);
+    socket.on('disconnect', handleDisconnect);
+
+    return () => {
+      socket.off('connect', handleConnect);
+      socket.off('disconnect', handleDisconnect);
+    };
+  }, []);
+
+  return <></>;
+};
