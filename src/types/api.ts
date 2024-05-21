9ef30d356395cdb6dfbff6cc3d8c148a705d2ba4
sangpok <48979587+sangpok@users.noreply.github.com>
2024-05-21 21:31:41 +0900

    🏷️ Fetcher 및 API 타입 변경 및 작성 (#62)
    API 성공/실패 응답 및 네트워크 에러/알 수 없는 에러 타입 작성

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,12 +1,6 @@
-export type SuccessResponse<T> = {
-  status: number;
-  payload: T;
-};
+export type SuccessResponse<T> = T;
 
-export type ErrorResponse = {
-  status: number;
-  payload: ErrorObject;
-};
+export type ErrorResponse = ErrorObject;
 
 export type ErrorObject = {
   errorCode: number;
@@ -15,4 +9,44 @@ export type ErrorObject = {
 
 export type APIResponse<T> = SuccessResponse<T> | ErrorResponse;
 
-export type EmptyResponse = null;
+export enum HTTPMethod {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+  PATCH = 'patch',
+}
+
+export enum FetchStatus {
+  SUCCESS = 'success',
+  FAIL = 'fail',
+  NETWORK_ERROR = 'network_error',
+  UNKNOWN_ERROR = 'unknown_error',
+}
+
+export type FetchSuccessResponse<T> = {
+  status: FetchStatus.SUCCESS;
+  data: T;
+};
+
+export type FetchFailResponse = {
+  status: FetchStatus.FAIL;
+  data: ErrorResponse;
+};
+
+/** Network Error는 TypeError를 반환 */
+export type FetchNetworkErrorResponse = {
+  status: FetchStatus.NETWORK_ERROR;
+  data: Error;
+};
+
+export type FetchUnknownErrorResponse = {
+  status: FetchStatus.UNKNOWN_ERROR;
+  data: string;
+};
+
+export type FetchResponse<T> =
+  | FetchSuccessResponse<T>
+  | FetchFailResponse
+  | FetchNetworkErrorResponse
+  | FetchUnknownErrorResponse;
